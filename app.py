474f23e3a12f9510f6b75b07ff4c1d79cf1359d0
tyler <tylercobb92@gmail.com>
2022-01-22 18:31:58 -0500

    routes for questions, answers, and thank you added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -14,3 +14,35 @@ responses = []
 @app.route('/')
 def start_survey():
     return render_template('start_survey.html', survey=survey)
+
+
+@app.route('/questions/<int:question_id>')
+def show_question(question_id):
+    if (responses is None):
+        return redirect('/')
+
+    if(len(responses) == len(survey.questions)):
+        return redirect('/complete')
+
+    if(len(responses) != question_id):
+        return redirect(f'/questions/{len(responses)}')
+
+    question = survey.questions[question_id]
+    return render_template('question.html', question_id=question_id, question=question)
+
+
+@app.route('/answer', methods=["POST"])
+def get_answer():
+    answer = request.form['choice']
+    responses.append(answer)
+
+    if(len(responses) == len(survey.questions)):
+        return redirect('/complete')
+
+    else:
+        return redirect(f'/questions/{len(responses)}')
+
+
+@app.route('/complete')
+def finished_survey():
+    return render_template('complete.html')
