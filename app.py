c3bdb7f04487e59ae47afce10d8e2cf1ce0d76b5
tyler <tylercobb92@gmail.com>
2022-01-22 20:27:17 -0500

    flash added to def show_question to prevent invalid id from being requested

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "onomatopoeia"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -25,6 +26,7 @@ def show_question(question_id):
         return redirect('/complete')
 
     if(len(responses) != question_id):
+        flash(f'Question ID {question_id} is invalid')
         return redirect(f'/questions/{len(responses)}')
 
     question = survey.questions[question_id]
